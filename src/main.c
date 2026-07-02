36f671b08ce923c27bbcbfc3400311f26bb21c36
Jiri Machula <jirikmachula@proton.me>
2026-07-02 16:16:22 +0200

    uz facha convert, save, open, treshold

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@ GdkPixbuf *imidz_pojntr;
 GtkWidget *window;
 GtkWidget *box;
 GtkWidget *imidztukonvert;
-
+GtkWidget *slider;
 
 
 
@@ -43,43 +43,65 @@ void selectfile(GtkButton *button3, gpointer user_data) {
 
 }
 
+
 void convert(GtkButton *button1, gpointer user_data) {
     if (imidz_pojntr) {
         if (imidztukonvert) {
             gtk_widget_destroy(imidztukonvert);
             imidztukonvert = NULL;
         }
+        int val = gtk_range_get_value(GTK_RANGE(slider));
         GdkPixbuf *saturated = gdk_pixbuf_copy(imidz_pojntr);
         gdk_pixbuf_saturate_and_pixelate(imidz_pojntr, saturated, 0.0, FALSE);
+        guchar *p = gdk_pixbuf_get_pixels(saturated);
+        int size = gdk_pixbuf_get_height(saturated) * gdk_pixbuf_get_rowstride(saturated);
+        for (int i = 0; i < size; i++) {
+            p[i] = (p[i] > val) ? 255 : 0;
+        }
         imidztukonvert = gtk_image_new_from_pixbuf(saturated);
         gtk_box_pack_start(GTK_BOX(box), imidztukonvert, TRUE, TRUE, 50);
         gtk_widget_show_all(window);
-        g_object_unref(saturated);
     }
 }
 
+void save(GtkButton *button4, gpointer user_data) {
+    if (imidztukonvert) {
+        GError *err = NULL;
+        GdkPixbuf *pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(imidztukonvert));
+        gdk_pixbuf_save(pixbuf, "out.jpg", "jpeg", &err, "quality", "95", NULL);
+    }
+}
 
+/*void export(GtkButton *button5, gpointer user_data) {
+    GdkPixbuf *pixbuf = gtk_image_get_pixbuf(GTK_IMAGE(imidztukonvert));
+    int width = gdk_pixbuf_get_width(pixbuf);
+    int 
+
+
+}
+*/
 int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
-
-
-    
     GtkWidget *button1;
     GtkWidget *button2;
     GtkWidget *button3;
-    
-
+    GtkWidget *button4;
+    GtkWidget *button5;
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     button1 = gtk_button_new_with_label("Convert!");
     button2 = gtk_button_new_with_label("Exit");
     button3 = gtk_button_new_with_label("Open File");
+    button4 = gtk_button_new_with_label("Save image");
+    button5 = gtk_button_new_with_label("Export file for NXT");
+    slider = gtk_scale_new_with_range(GTK_ORIENTATION_HORIZONTAL, 0, 255, 1);
     box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
 
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(button1, "clicked", G_CALLBACK(convert), NULL);
     g_signal_connect(button2, "clicked", G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(button3, "clicked", G_CALLBACK(selectfile), NULL);
+    g_signal_connect(button4, "clicked", G_CALLBACK(save), NULL);
 
 
     gtk_window_set_title(GTK_WINDOW(window), "Konverter obrazku do souradnic pro robota");
@@ -91,7 +113,10 @@ int main(int argc, char *argv[]) {
     gtk_box_pack_start(GTK_BOX(box), button1, TRUE, TRUE, 50);
     gtk_box_pack_start(GTK_BOX(box), button2, TRUE, TRUE, 50);
     gtk_box_pack_start(GTK_BOX(box), button3, TRUE, TRUE, 50);
+    gtk_box_pack_start(GTK_BOX(box), button4, TRUE, TRUE, 50);
+    gtk_box_pack_start(GTK_BOX(box), slider, TRUE, TRUE, 50);
 
+    
     gtk_widget_show_all(window);
 
     gtk_main();
